f065040fb2e0ef375b31e22d6cc975cc097e4448
ayanantadey <119517487+ayanantadey@users.noreply.github.com>
2022-11-30 21:28:52 +0530

    Update ayananta.py

--- ayananta.py
+++ ayananta.py
@@ -13,13 +13,10 @@ def main():
   st.markdown(html_temp,unsafe_allow_html=True)
   num1 = st.number_input("Number 1")
   num2 = st.number_input("Number 2")
-  
   if(num1==0 or num2==0):
     result = 0
   else:
     result=num1/num2
-  
-  st.success('The output is {}'.format(result))
-  
+  st.success('The output is {}'.format(result))  
 if __name__and__main__:
   main()
