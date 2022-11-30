ac338445c87374eb34ea3fddb0e094b6a4cee3f0
ayanantadey <119517487+ayanantadey@users.noreply.github.com>
2022-11-30 21:42:35 +0530

    Update ayananta.py

--- ayananta.py
+++ ayananta.py
@@ -1,5 +1,25 @@
-a=int(input())
-if a%2==1:
-  print('Odd')
-else:
-  print('Even')
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+def main():
+  st.title("Division of Two Numbers")
+  html_temp = """
+  <div style="background-color:black;padding:10px">
+  <h2 style="color:black;text-align:center;">Division of 2 Numbers</h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  num1 = st.number_input("Number 1")
+  num2 = st.number_input("Number 2")
+  
+  if(num1==0 or num2==0):
+    result = 0
+  else:
+    result=num1/num2
+  
+  st.success('The output is {}'.format(result))
+  
+if _name_=='_main_':
+  main()
