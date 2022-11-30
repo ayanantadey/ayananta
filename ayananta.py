3ec5e1830e64d3631c8efb1ef6366372f76540a3
ayanantadey <119517487+ayanantadey@users.noreply.github.com>
2022-11-30 22:06:50 +0530

    Update ayananta.py

--- ayananta.py
+++ ayananta.py
@@ -4,20 +4,19 @@ import streamlit as st
 
 
 def main():
-  st.title("Division of Two Numbers")
+  st.title("Odd Even")
   html_temp = """
   <div style="background-color:black;padding:10px">
   <h2 style="color:black;text-align:center;">Division of 2 Numbers</h2>
   </div>
   """
   st.markdown(html_temp,unsafe_allow_html=True)
-  num1 = st.number_input("Number 1")
-  num2 = st.number_input("Number 2")
+  num1 = st.number_input("Enter Number")
   
-  if(num1==0 or num2==0):
-    result = 0
+  if(num1%2==0):
+    result = 'Even'
   else:
-    result=num1/num2
+    result='Odd'
     
   st.success('The output is {}'.format(result))
   
